90b207fd9a8c41ab8a1313275ac04ab5956364ab
Justin Spedding <jspedding@chathamfinancial.com>
2020-02-10 16:54:08 -0500

    make it a calculator :)

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload.2
         </p>
         <a
           className="App-link"
@@ -18,9 +18,60 @@ function App() {
         >
           Learn React
         </a>
+        <Calculator></Calculator>
       </header>
     </div>
   );
+}
+
+class Calculator extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      num1: '',
+      num2: '',
+      result: '',
+    };
+  }
+  handleClick() {
+    //do something
+  }
+  
+  handleChange = (e) => {
+    this.setState({
+       [e.target.id]: e.target.value
+    });
+  }
+
+  calculate = (e) => {
+    fetch(`/api/${e.target.id}/${this.state.num1}/${this.state.num2}`, {
+      method: 'GET',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+      },
+    })
+    .then(res => res.json())
+    .then((data) => {
+      this.setState({ result: data.result });
+    })
+    .catch(console.log);
+  }
+
+  render() {
+    return (
+    <div>
+      <div>Number 1: <input id="num1" type="text" onChange={e => this.handleChange(e)}/></div>
+      <div>Number 2: <input id="num2" type="text" onChange={e => this.handleChange(e)}/></div>
+      <div>
+        <button id="add" type="submit" onClick={this.calculate}>Add</button>
+        <button id="subtract" type="submit" onClick={this.calculate}>Subtract</button>
+        <button id="multiply" type="submit" onClick={this.calculate}>Multiply</button>
+        <button id="divide" type="submit" onClick={this.calculate}>Divide</button>
+      </div>
+      <div>Result: {this.state.result}</div>
+    </div>)
+  }
 }
 
 export default App;
